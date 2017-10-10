525b6490e09110badba7353522ac5eec2bca2464
Joe He <joe.he@d1m.cn>
2017-10-10 16:44:00 +0800

    update

--- getDocFile.bat
+++
@@ -1,4 +0,0 @@
-c:
-cd C:\Users\D1M\Desktop\properties_manager
-java -jar properties-manager.jar
-
--- update properties .bat
+++ update properties .bat
@@ -1,7 +1,8 @@
 set CCFE_HOME=D:\WorkSpace\ccfe-backend
+d:
 
-start cmd /c "d: && cd %CCFE_HOME%\ccfe-env\DTC\Local && mongo 10.12.121.244:27018/ccfe_prop_Local --shell region_brand.js properties_bundle.js role.js ccfe_regular_init.js && exit"
-start cmd /c "d: && cd %CCFE_HOME%\ccfe-env\DTC\Staging && mongo 10.12.121.244:27018/ccfe_prop_Staging --shell region_brand.js properties_bundle.js role.js ccfe_regular_init.js && exit"
-start cmd /c "d: && cd %CCFE_HOME%\ccfe-env\DTC\Preprod && mongo 10.12.121.244:27018/ccfe_prop_Preprod --shell region_brand.js properties_bundle.js role.js ccfe_regular_init.js && exit"
-start cmd /c "d: && cd %CCFE_HOME%\ccfe-env\DTC\Prod && mongo 10.12.121.244:27018/ccfe_prop_Prod --shell region_brand.js properties_bundle.js role.js ccfe_regular_init.js && exit"
+start cmd /c "cd %CCFE_HOME%\ccfe-env\DTC\Local && mongo 10.12.121.244:27018/ccfe_prop_Local --shell region_brand.js properties_bundle.js role.js ccfe_regular_init.js && exit"
+start cmd /c "cd %CCFE_HOME%\ccfe-env\DTC\Staging && mongo 10.12.121.244:27018/ccfe_prop_Staging --shell region_brand.js properties_bundle.js role.js ccfe_regular_init.js && exit"
+start cmd /c "cd %CCFE_HOME%\ccfe-env\DTC\Preprod && mongo 10.12.121.244:27018/ccfe_prop_Preprod --shell region_brand.js properties_bundle.js role.js ccfe_regular_init.js && exit"
+start cmd /c "cd %CCFE_HOME%\ccfe-env\DTC\Prod && mongo 10.12.121.244:27018/ccfe_prop_Prod --shell region_brand.js properties_bundle.js role.js ccfe_regular_init.js && exit"
 
